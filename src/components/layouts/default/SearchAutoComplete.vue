5858c607282c19bf5bbf315f4568b48c68359894
bhupendra singh <yuvibana03@gmail.com>
2024-06-21 00:20:22 +0530

    footer created

--- src/components/layouts/default/SearchAutoComplete.vue
+++ src/components/layouts/default/SearchAutoComplete.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <h1>Hello AutoComplete</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
-
-<style lang="scss">
-/* Example scoped styles */
-</style>
+    <div>
+      <h1>Hello AutoComplete</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style lang="scss">
+  /* Example scoped styles */
+  </style>
+  
